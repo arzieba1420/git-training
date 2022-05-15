0bb14fa426f4f67971d2e7372e7ced20668cc256
arzieba1420 <arzieba1420@wszib.edu.pl>
2022-05-15 15:58:49 +0200

    hello Arek

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Poland!");
+        System.out.println("Hello Arek!");
     }
 
     private void displayText(){
